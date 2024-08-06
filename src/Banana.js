36139d59ee16b4fbab5bd2e019d1b71d5df252d9
jinsil-kim <wlstlf9315@gmail.com>
2024-08-06 15:44:30 +0900

    Add files via upload

---
+++ src/Banana.js
@@ -0,0 +1,5 @@
+const Banana = () => {
+  return <div>바나나🍌</div>;
+};
+
+export default Banana;
---
+++ src/Pineapple.js
@@ -0,0 +1,5 @@
+const Pineapple = () => {
+  return <div>파인애플🍍</div>;
+};
+
+export default Pineapple;

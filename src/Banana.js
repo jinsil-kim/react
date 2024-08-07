5bd13abcdbd00c36880c4c7667d22c3da6cce36f
jinsil-kim <wlstlf9315@gmail.com>
2024-08-07 14:50:06 +0900

    Delete src directory

--- src/Banana.js
+++
@@ -1,5 +0,0 @@
-const Banana = () => {
-  return <div>바나나🍌</div>;
-};
-
-export default Banana;
--- src/Pineapple.js
+++
@@ -1,5 +0,0 @@
-const Pineapple = () => {
-  return <div>파인애플🍍</div>;
-};
-
-export default Pineapple;

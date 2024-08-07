2a46778df07893581aa3d3fa10b2e857a454e66e
jinsil-kim <wlstlf9315@gmail.com>
2024-08-07 14:51:22 +0900

    Add files via upload

---
+++ Day2/Box.js
@@ -0,0 +1,13 @@
+const Box = () => {
+  //1. css를 js화 할때는 중괄호 두개 필요
+
+  const boxStyle = {
+    width: '100px',
+    height: '100px',
+    border: '1px solid black',
+  };
+
+  return <div style={boxStyle1}></div>;
+};
+
+export default Box;

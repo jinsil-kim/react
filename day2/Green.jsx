2dd5521407d8aba5d34954cb63064c028aef83dc
jinsil-kim <wlstlf9315@gmail.com>
2024-08-09 15:46:47 +0900

    Add files via upload

---
+++ day2/Green.jsx
@@ -0,0 +1,11 @@
+const Green = () => {
+  const greenStyle = {
+    width: '100px',
+    height: '100px',
+    border: '1px solid green',
+  };
+
+  return <div style={greenStyle}></div>;
+};
+
+export default Green;
---
+++ day2/Yellow.jsx
@@ -0,0 +1,12 @@
+const Yellow = () => {
+  const yellowStyle = {
+    width: '100px',
+    height: '100px',
+    border: '1px solid yellow',
+    borderRadius: '999px',
+  };
+
+  return <div style={yellowStyle}></div>;
+};
+
+export default Yellow;

dfc4ce766213f0e1da3ffe6adc90c143716500ff
jinsil-kim <wlstlf9315@gmail.com>
2024-08-13 16:06:40 +0900

    Add files via upload

---
+++ day6/App.jsx
@@ -0,0 +1,12 @@
+import Box from "./day6/Box";
+import Circle from "./day6/Circle";
+
+function App() {
+  return (
+    <>
+      <Box></Box>
+      <Circle name="hi"></Circle>
+    </>
+  );
+}
+export default App;

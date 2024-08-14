8222980d700384a77cb26217a2ac68b437edb654
jinsil-kim <wlstlf9315@gmail.com>
2024-08-14 16:44:12 +0900

    Add files via upload

---
+++ Practice/App.jsx
@@ -0,0 +1,20 @@
+import Fourth from "./Practice/Fourth";
+import Header from "./Practice/Header";
+import Look from "./Practice/Look";
+import Notice from "./Practice/Notice";
+import Refund from "./Practice/Refund";
+import Request from "./Practice/Request";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Request />
+      <Look />
+      <Refund />
+      <Fourth />
+      <Notice />
+    </div>
+  );
+}
+export default App;

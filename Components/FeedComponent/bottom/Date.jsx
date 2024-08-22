51b5744b420f1ade9d099e0b450595e77b7aa5e0
jinsil-kim <wlstlf9315@gmail.com>
2024-08-22 14:23:51 +0900

    Delete Components directory

--- Components/FeedComponent/bottom/Date.jsx
+++
@@ -1,9 +0,0 @@
-const Date = (props) => {
-  return (
-    <div>
-      <span className="text-gray-300">{props.date || "No Montyh Date"}</span>
-    </div>
-  );
-};
-
-export default Date;
--- Components/FeedComponent/headerfeed/PlaceLabel.jsx
+++
@@ -1,9 +0,0 @@
-const PlaceLabel = (props) => {
-  return (
-    <span className="text-xs">
-      {props.city || "City"}, {props.nation || "Korea"}
-    </span>
-  );
-};
-
-export default PlaceLabel;
